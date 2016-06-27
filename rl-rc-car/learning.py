099dcbf4f6ff99ac70d8244ce638d32c63658d1a
Matt Harvey <harveym@gmail.com>
2016-06-26 19:29:37 -0700

    Update learning to use Project Becho.

--- rl-rc-car/learning.py
+++ rl-rc-car/learning.py
@@ -1,177 +1,72 @@
-from flat_game import carmunk
-import numpy as np
-import random
-import csv
-from nn import neural_net, LossHistory
-import timeit
-
-NUM_INPUTS = 3
-GAMMA = 0.9  # Forgetting.
-
-
-def train_net(model, params):
-
-    filename = params_to_filename(params)
-
-    observe = 1000  # Number of frames to observe before training.
-    epsilon = 1
-    train_frames = 250000  # Number of frames to play.
-    batchSize = params['batchSize']
-    buffer = params['buffer']
-
-    # Just stuff used below.
-    max_car_distance = 0
-    car_distance = 0
-    t = 0
-    data_collect = []
-    replay = []  # stores tuples of (S, A, R, S').
-    loss_log = []
-
-    # Create a new game instance.
-    game_state = carmunk.GameState()
-
-    # Get initial state by doing nothing and getting the state.
-    _, state = game_state.frame_step((2))
-
-    # Let's time it.
-    start_time = timeit.default_timer()
-
-    # Run the frames.
-    while t < train_frames:
-
-        t += 1
-        car_distance += 1
-
-        # Choose an action.
-        if random.random() < epsilon or t < observe:
-            action = np.random.randint(0, 3)  # random
-        else:
-            # Get Q values for each action.
-            qval = model.predict(state, batch_size=1)
-            action = (np.argmax(qval))  # best
-
-        # Take action, observe new state and get our treat.
-        reward, new_state = game_state.frame_step(action)
-
-        # Experience replay storage.
-        replay.append((state, action, reward, new_state))
-
-        # If we're done observing, start training.
-        if t > observe:
-
-            # If we've stored enough in our buffer, pop the oldest.
-            if len(replay) > buffer:
-                replay.pop(0)
-
-            # Randomly sample our experience replay memory
-            minibatch = random.sample(replay, batchSize)
-
-            # Get training values.
-            X_train, y_train = process_minibatch(minibatch, model)
-
-            # Train the model on this batch.
-            history = LossHistory()
-            model.fit(
-                X_train, y_train, batch_size=batchSize,
-                nb_epoch=1, verbose=0, callbacks=[history]
-            )
-            loss_log.append(history.losses)
-
-        # Update the starting state with S'.
-        state = new_state
-
-        # Decrement epsilon over time.
-        if epsilon > 0.1 and t > observe:
-            epsilon -= (1/train_frames)
-
-        # We died, so update stuff.
-        if reward == -500:
-            # Log the car's distance at this T.
-            data_collect.append([t, car_distance])
-
-            # Update max.
-            if car_distance > max_car_distance:
-                max_car_distance = car_distance
-
-            # Time it.
-            tot_time = timeit.default_timer() - start_time
-            fps = car_distance / tot_time
-
-            # Output some stuff so we can watch.
-            print("Max: %d at %d\tepsilon %f\t(%d)\t%f fps" %
-                  (max_car_distance, t, epsilon, car_distance, fps))
-
-            # Reset.
-            car_distance = 0
-            start_time = timeit.default_timer()
-
-        # Save the model every 25,000 frames.
-        if t % 25000 == 0:
-            model.save_weights('saved-models/' + filename + '-' +
-                               str(t) + '.h5',
-                               overwrite=True)
-            print("Saving model %s - %d" % (filename, t))
-
-    # Log results after we're done all frames.
-    log_results(filename, data_collect, loss_log)
-
-
-def log_results(filename, data_collect, loss_log):
-    # Save the results to a file so we can graph it later.
-    with open('results/sonar-rc/learn_data-' + filename + '.csv', 'w') as data_dump:
-        wr = csv.writer(data_dump)
-        wr.writerows(data_collect)
-
-    with open('results/sonar-rc/loss_data-' + filename + '.csv', 'w') as lf:
-        wr = csv.writer(lf)
-        for loss_item in loss_log:
-            wr.writerow(loss_item)
-
-
-def process_minibatch(minibatch, model):
-    """This does the heavy lifting, aka, the training. It's super jacked."""
-    X_train = []
-    y_train = []
-    # Loop through our batch and create arrays for X and y
-    # so that we can fit our model at every step.
-    for memory in minibatch:
-        # Get stored values.
-        old_state_m, action_m, reward_m, new_state_m = memory
-        # Get prediction on old state.
-        old_qval = model.predict(old_state_m, batch_size=1)
-        # Get prediction on new state.
-        newQ = model.predict(new_state_m, batch_size=1)
-        # Get our best move. I think?
-        maxQ = np.max(newQ)
-        y = np.zeros((1, 3))
-        y[:] = old_qval[:]
-        # Check for terminal state.
-        if reward_m != -500:  # non-terminal state
-            update = (reward_m + (GAMMA * maxQ))
-        else:  # terminal state
-            update = reward_m
-        # Update the value for the action we took.
-        y[0][action_m] = update
-        X_train.append(old_state_m.reshape(NUM_INPUTS,))
-        y_train.append(y.reshape(3,))
-
-    X_train = np.array(X_train)
-    y_train = np.array(y_train)
-
-    return X_train, y_train
-
-
-def params_to_filename(params):
-    return str(params['nn'][0]) + '-' + str(params['nn'][1]) + '-' + \
-            str(params['batchSize']) + '-' + str(params['buffer'])
+from becho import becho, bechonet
+from sim import carmunk
+from statistics import mean
 
 
 if __name__ == "__main__":
-        nn_param = [1000, 1000]
-        params = {
-            "batchSize": 400,
-            "buffer": 50000,
-            "nn": nn_param
-        }
-        model = neural_net(NUM_INPUTS, nn_param)
-        train_net(model, params)
+    frames = 2000
+    inputs = 3
+    actions = 3
+
+    # Just change these.
+    train_or_show = 'train'
+    weights_file = ''
+
+    if train_or_show == 'train':
+        enable_training = True
+        load_weights = False
+        save_weights = True
+    else:
+        enable_training = False
+        load_weights = True
+        save_weights = False
+
+    network = bechonet.BechoNet(num_actions=actions, num_inputs=inputs,
+                                nodes_1=512, nodes_2=512, verbose=True,
+                                load_weights=load_weights,
+                                weights_file=weights_file,
+                                save_weights=save_weights)
+    pb = becho.ProjectBecho(network, frames=frames, num_actions=actions,
+                            batch_size=32, min_epsilon=0.1, num_inputs=inputs,
+                            replay_size=1000000, gamma=0.99, verbose=True,
+                            enable_training=enable_training)
+
+    rewards = []
+    results = []
+    max_steps = 2000
+    repeat_action = 2
+
+    game_state = carmunk.GameState()
+
+    distances = []
+    results = []
+
+    distance = 0
+
+    for i in range(frames):
+        _, state = game_state.frame_step((2))
+
+        distance += 1
+        action = pb.get_action(state)
+
+        reward, new_state = game_state.frame_step(action)
+
+        pb.step(state, action, reward, new_state, False)
+
+        if reward == -500:
+            # Give us some info.
+            print(distance)
+            distances.append(distance)
+            if len(distances) > 100:
+                distances.pop(0)
+            distance = 0
+
+        state = new_state
+
+        if i % 100 == 0 and i > 0:
+            print("Epsilon: %.5f" % pb.epsilon)
+            print("%d - Average distance: %.2f" % (i, mean(distances)))
+            results.append(mean(distances))
+
+    for r in results:
+        print(r)
