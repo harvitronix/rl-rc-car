526ce9f10ca5d1e460e9719868826ea9ac134fac
Matt Harvey <harveym@gmail.com>
2016-06-26 18:36:22 -0700

    Restructure project.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='RL RC Car',
+    version='0.1dev',
+    packages=['rl-rc-car'],
+    license='The MIT License (MIT)',
+    long_description=open('README.md').read(),
+)
