baed127713be65a13c3d1a6182c827bf7d5ca154
Matt Harvey <harveym@gmail.com>
2016-07-02 15:50:43 -0700

    Fix sensor cleanup.

--- rl-rc-car/cleanup.py
+++ rl-rc-car/cleanup.py
@@ -4,5 +4,7 @@ from sensors import Sensors
 car = RCCar()
 car.cleanup_gpio()
 
-sensors = Sensors()
+ir_pins = [24, 21]
+sonar_pins = [[25, 8]]
+sensors = Sensors(ir_pins, sonar_pins)
 sensors.cleanup_gpio()
