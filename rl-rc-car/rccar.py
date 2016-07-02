a55b4fd36e082f7d1c63da7626d89c990f6a0fab
Matt Harvey <harveym@gmail.com>
2016-07-02 14:00:51 -0700

    Make driver class more like an actual object.

--- rl-rc-car/rccar.py
+++ rl-rc-car/rccar.py
@@ -5,31 +5,28 @@ version of the carmunk simulation.
 import RPi.GPIO as GPIO
 import time
 
-# Constants
-LEFT_PIN = 13
-RIGHT_PIN = 15
-FORWARD_PIN = 12
-BACKWARD_PIN = 11
-
-# Time in seconds to run each action.
-# 0 = continuous.
-APPLY_TIME = 0.3
-
 
 class RCCar:
-    def __init__(self):
+    def __init__(self, left_p=13, right_p=15,
+                 forward_p=12, backward_p=11,
+                 apply_time=0.3):
+        self.left_p = left_p
+        self.right_p = right_p
+        self.forward_p = forward_p
+        self.backward_p = backward_p
+
         print("Setting up GPIO pins.")
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(BACKWARD_PIN, GPIO.OUT)  # Backwards.
-        GPIO.setup(FORWARD_PIN, GPIO.OUT)  # Forwards.
-        GPIO.setup(LEFT_PIN, GPIO.OUT)  # Left.
-        GPIO.setup(RIGHT_PIN, GPIO.OUT)  # Right.
+        GPIO.setup(self.backward_p, GPIO.OUT)  # Backwards.
+        GPIO.setup(self.forward_p, GPIO.OUT)  # Forwards.
+        GPIO.setup(self.left_p, GPIO.OUT)  # Left.
+        GPIO.setup(self.right_p, GPIO.OUT)  # Right.
 
         # Reset in case they're still on from before.
-        GPIO.output(BACKWARD_PIN, 0)
-        GPIO.output(FORWARD_PIN, 0)
-        GPIO.output(LEFT_PIN, 0)
-        GPIO.output(RIGHT_PIN, 0)
+        GPIO.output(self.backward_p, 0)
+        GPIO.output(self.forward_p, 0)
+        GPIO.output(self.left_p, 0)
+        GPIO.output(self.right_p, 0)
 
     def cleanup_gpio(self):
         print("Cleaning up GPIO pins.")
@@ -48,34 +45,34 @@ class RCCar:
         # Forward or back.
         if 0 <= action <= 2:
             # Forwards.
-            GPIO.output(BACKWARD_PIN, 0)
-            GPIO.output(FORWARD_PIN, 1)
+            GPIO.output(self.backward_p, 0)
+            GPIO.output(self.forward_p, 1)
         else:
             # Backwards.
-            GPIO.output(FORWARD_PIN, 0)
-            GPIO.output(BACKWARD_PIN, 1)
+            GPIO.output(self.forward_p, 0)
+            GPIO.output(self.backward_p, 1)
 
         # Turning.
         if action == 0 or action == 3:  # Turn right.
-            GPIO.output(LEFT_PIN, 0)
-            GPIO.output(RIGHT_PIN, 1)
+            GPIO.output(self.left_p, 0)
+            GPIO.output(self.right_p, 1)
             print("Turning right.")
         elif action == 1 or action == 4:  # Turn left.
-            GPIO.output(RIGHT_PIN, 0)
-            GPIO.output(LEFT_PIN, 1)
+            GPIO.output(self.right_p, 0)
+            GPIO.output(self.left_p, 1)
             print("Turning left.")
         else:
-            GPIO.output(LEFT_PIN, 0)
-            GPIO.output(RIGHT_PIN, 0)
+            GPIO.output(self.left_p, 0)
+            GPIO.output(self.right_p, 0)
             print("Going straight.")
 
         # Pausing.
-        if APPLY_TIME > 0:
-            time.sleep(APPLY_TIME)
+        if self.apply_time > 0:
+            time.sleep(self.apply_time)
 
         # Temporarily stop motion here. Tries to slow things down.
-        GPIO.output(BACKWARD_PIN, 0)
-        GPIO.output(FORWARD_PIN, 0)
+        GPIO.output(self.backward_p, 0)
+        GPIO.output(self.forward_p, 0)
 
     def proximity_alert(self, readings):
         if readings[0] == 0 or readings[2] == 0 or readings[1] < 5:
