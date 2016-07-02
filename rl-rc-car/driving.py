70ced4934a539545514a2e037dfbaf032158a1b7
Matt Harvey <harveym@gmail.com>
2016-07-02 16:11:05 -0700

    New model.

--- rl-rc-car/driving.py
+++ rl-rc-car/driving.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
         num_actions=6, num_inputs=3,
         nodes_1=256, nodes_2=256, verbose=True,
         load_weights=True,
-        weights_file='saved-models/sonar-and-ir-9750.h5'
+        weights_file='saved-models/sonar-and-ir-3750.h5'
     )
     pb = becho.ProjectBecho(
         network, num_actions=6, num_inputs=3,
         verbose=True, enable_training=False
     )
-    car = RCCar(apply_time=0.3, wait_time=0.5)
+    car = RCCar(apply_time=0.2, wait_time=0.5)
     sensors = SensorClient()
 
     input("Ready to roll! Press any key to go.")
@@ -28,8 +28,10 @@ if __name__ == '__main__':
         action = pb.get_action(readings)
         car.step(action)
 
+        print(readings)
+
         if car.proximity_alert(readings):
-            print('Crashed!')
+            print('Proximity alert!')
 
         print("-"*80)
 
