befc0fea68c619c2bf99cde55d41b3f730b21cae
Matt Harvey <harveym@gmail.com>
2016-07-16 20:28:24 -0700

    Update sensor server.

--- rl-rc-car/sensor_server.py
+++ rl-rc-car/sensor_server.py
@@ -26,13 +26,13 @@ class SensorServer:
         ir_s = self.r_server.get('ir_s')
         ir_l = self.r_server.get('ir_l')
         ir_r = self.r_server.get('ir_r')
-        ir_s = self.r_server.get('s_m')
+        s_m = json.loads(self.r_server.get('s_m'))
 
         readings = {
             'ir_s': ir_s,
             'ir_l': ir_l,
             'ir_r': ir_r,
-            'ir_s': ir_s
+            's_m': s_m
         }
 
         client, address = self.s.accept()
